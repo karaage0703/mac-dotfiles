4688fdfeeccd00393f645be760e929dbffc3b479
karaage0703 <coldchicken@gmail.com>
2016-03-28 21:58:02 +0900

    change from copy to symbolic link .bashrc .bash_profile

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# install brew package
 brew install pyenv
 brew install pyenv-virtualenv
 brew install byobu
+
+# make symbolic link
+ln -sf ~/mac-dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/mac-dotfiles/.bashrc ~/.bashrc
