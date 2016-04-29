af7437725570feb439b8a8a064231e53a8287738
karaage <coldchicken@gmail.com>
2016-04-29 14:25:21 +0900

    add packages

--- homebrew.sh
+++ homebrew.sh
@@ -13,16 +13,16 @@ fi
 brew install pyenv
 brew install pyenv-virtualenv
 
-# Byobu (virtual terminal)
+# byobu (virtual terminal)
 brew install byobu
 
-# Vim
+# editor
 brew install vim
 
-# Git
+# git
 brew install git
 
-# Pandoc
+# convert markdown to doc
 brew install pandoc
 
 # wget
@@ -34,6 +34,10 @@ brew install ctags
 # From here Homebrew Cask
 # install Cask
 brew tap caskroom/cask
+brew tap caskroom/homebrew-versions
+
+# editor
+brew cask install macvim
 
 # editor
 brew cask install atom
@@ -41,8 +45,47 @@ brew cask install atom
 # drawing
 brew cask install skitch
 
-# burn dvd
+# burning dvd tool
 brew cask install burn
 
-# open office
-burn cask install openoffice
+# clipboard
+brew cask install clipmenu
+
+# free office software
+brew cask install openoffice
+
+# web browser
+brew cask install google-chrome
+
+# web browser
+brew cask install firefox-ja
+
+# file sharing
+brew cask install dropbox
+
+# memo sharing
+brew cask install evernote
+
+# graphic
+brew cask install gimp
+
+# delete exif information
+brew cask install imageoptim
+
+# video player
+brew cask install vlc
+
+# audio programming
+brew cask install sonic-pi
+
+# file sharing
+brew cask install google-drive
+
+# brief circuit and board editor
+brew cask install fritzing
+
+# sftp software
+brew cask install filezilla
+
+# midi composer
+brew cask install aria-maestosa
