1af652b70f5eb18b40cf303fc94659b7eb5ea9c8
karaage0703 <coldchicken@gmail.com>
2021-02-06 23:56:50 +0900

    Update for Apple silicon mac

--- defaults.sh
+++ defaults.sh
@@ -14,8 +14,15 @@ defaults write com.apple.finder ShowStatusBar -bool true
 
 killall Finder
 
-printf "delete Japanese file name of screen shot"
+printf "delete Japanese file name of screen shot\n"
 defaults write com.apple.screencapture name ""
 
-printf "Don't make .DS_Store on network volume"
+printf "Don't make .DS_Store on network volume\n"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+printf "Continious key press\n"
+defaults write -g ApplePressAndHoldEnabled -bool false # mac
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false # VS Code
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabljed -bool false # VS Code Insiders
+# Reference: https://marketplace.visualstudio.com/items?itemName=vscodevim.vim
+# Reference: https://discussionsjapan.apple.com/thread/10158407
--- homebrew.sh
+++ homebrew.sh
@@ -11,8 +11,7 @@ brew install pyenv-virtualenv
 brew install byobu
 
 # editor
-brew install lua
-brew install vim --with-lua
+brew install vim
 
 # git
 brew install git
@@ -23,97 +22,93 @@ brew install pandoc
 # file downloader
 brew install wget
 
-# function jump for programming
-brew install ctags
-
 # unarchiver
 brew install p7zip
 
 # FROM HERE HOMEBREW CASK Packages
 # Install Homebrew cask
-brew tap caskroom/cask
-brew tap caskroom/homebrew-versions		# for firefox-jp ext...
+# brew tap homebrew/cask
 
 # editor
-brew cask install macvim
-brew cask install visual-studio-code
+# brew install macvim
+# brew cask install visual-studio-code
 
 # binary editor
-brew cask install 0xed
+# brew cask install 0xed
 
 # drawing tool
-brew cask install skitch
+brew install skitch
 
 # burning dvd tool
-brew cask install burn
+# brew cask install burn
 
 # clipboard extender
-brew cask install clipy
+brew install clipy
 
 # editor
-brew cask install atom
+# brew cask install atom
 
 # free office software
-brew cask install libreoffice
+# brew cask install libreoffice
 
 # web browser
-brew cask install google-chrome
-brew cask install firefox
+# brew cask install google-chrome
+# brew cask install firefox
 
 # file/text sharing
-brew cask install dropbox
-brew cask install evernote
+# brew cask install dropbox
+# brew cask install evernote
 # brew cask install google-drive
 
 # graphic editor
-brew cask install gimp
+# brew cask install gimp
 
 # delete exif information tool
-brew cask install imageoptim
+brew install imageoptim
 
 # video player
-brew cask install vlc
+brew install vlc
 
 # audio programming
-brew cask install sonic-pi
+# brew cask install sonic-pi
 
 # midi composer
-brew cask install aria-maestosa
+# brew cask install aria-maestosa
 
 # circuit and board editor
-brew cask install fritzing		# simple
-brew cask install kicad				# high functionality
-brew cask install ltspice			# circuit simulator
+# brew cask install fritzing		# simple
+# brew cask install kicad				# high functionality
+# brew cask install ltspice			# circuit simulator
 
 # ftp/sftp software
-brew install lftp
+# brew install lftp
 
 # programming IDE
-brew cask install processing
-brew cask install arduino
+# brew cask install processing
+# brew cask install arduino
 
 # unarchiver
-brew cask install the-unarchiver
+# brew cask install the-unarchiver
 
 # synology
-brew tap caskroom/drivers
-brew cask install synology-cloud-station-drive
+# brew tap caskroom/drivers
+# brew cask install synology-cloud-station-drive
 
 # SD formatter
-brew cask install sdformatter
+brew install sdformatter
 
 # SD image writer
-brew cask install balenaetcher
+# brew cask install balenaetcher
 
 # Virtual Machine
-brew cask install virtualbox
+# brew cask install virtualbox
 
 # Docker
 brew install docker
-brew cask install docker
+# brew cask install docker
 
 # Hyper terminal
-brew cask install hyper
+# brew cask install hyper
 
 # Raspberry Pi Imager
-brew cask install raspberry-pi-imager
+brew install raspberry-pi-imager
